2f7d86fbf61e161f3c665ddcd36433d46b5fb3b6
Алексей <gogh78@mail.ru>
2022-02-12 19:52:01 +0400

    Добавьте файлы проекта.

---
+++ WpfEx19/Models/Сalculation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEx19.Models
+{
+    static class Сalculation
+    {
+        public static double Add(int r) => 2 * r * Math.PI;
+    }
+}
